c443f18377374e49149fd6254bbb96515d68451f
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2022-10-25 08:47:37 +0100

    updates

---
+++ inc/settings.php
@@ -0,0 +1,20 @@
+<?php
+//email settings for contact forms
+//Settings for all form scripts
+/// Define who the emails get sent to from forms filled out
+$email_to = "luke@boltonswindowcleaning.co.uk";
+
+$host = "mail.boltonswindowcleaning.co.uk"; /// Hostname
+$username = "admin@boltonswindowcleaning.co.uk"; ///Username
+$pass = "rKrltTuvlQ"; /// Password
+$from = $username; ///Email
+
+$fromname = "Webmaster - Bolton's Window Cleaning"; /// Username and how you want your name to be displayed on emails
+$emailheaderlogo = "https://www.boltonswindowcleaning.co.uk/assets/img/logo/logo-primary-col.png";//logo url for inserting into the top of email bodies
+
+//Contact details: Phone number, email address etc
+$contactnum ="07946 742580";
+$emailaddress = "luke@boltonswindowcleaning.co.uk";
+$facebookurl = "https://www.facebook.com/Boltons-Window-Cleaning-101418868199689";
+?>
+
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include("./inc/header.inc.php"); ?>
+<!-- Meta Tags For Each Page -->
+<meta name="description" content="">
+<meta name="title" content="">
+<!-- /Meta Tags -->
+
+<!-- / -->
+<!-- Page Title -->
+<title></title>
+<!-- /Page Title -->
+</head>
+
+<body>
+    <!-- Nav Bar -->
+    
+    <!-- /nav bar -->
+    <main class="main">
+        <!-- Main Body Of Page -->
+        <?php include("./inc/nav.inc.php"); ?>
+        <!-- /Main Body Of Page -->
+        
+    <!-- Footer -->
+    <?php include("./inc/footer.inc.php"); ?>
+    <!-- /Footer -->
+
+</body>
+
+</html>
